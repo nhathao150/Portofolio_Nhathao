eb05fc5129454248c28c06be87c2df84189d8e49
nhathao150 <65373388+nhathao150@users.noreply.github.com>
2025-08-29 01:04:27 +0700

    feat: Update typewriter effect text

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -209,7 +209,7 @@ const Home = () => {
                 <p className="text-base md:text-lg text-gray-400 max-w-xl leading-relaxed font-light"
                   data-aos="fade-up"
                   data-aos-delay="1000">
-                  Menciptakan Website Yang Inovatif, Fungsional, dan User-Friendly untuk Solusi Digital.
+                  Create innovative, usable and user-friendly websites for digital solutions.
                 </p>
 
                 {/* Tech Stack */}
